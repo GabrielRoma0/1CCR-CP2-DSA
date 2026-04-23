1566bfbd120681e0b2703f88eae73a8f6625f9ba
Laboratório FIAP <labsfiap@fiap.com.br>
2026-04-22 22:47:06 -0300

    adicionado prinft opções

--- src/main.c
+++ src/main.c
@@ -2,10 +2,28 @@
 
 int main() {
     int opcao;
-    
+    int nota_1;
+    int nota_2;
+    int media;
+    printf("==========\n");
+    printf("SISTEMA EQUIPE GTLF\n");
+    printf("==========\n");
+    printf("1 - Inserir notas:\n");
+    printf("2 - Calcular media:\n");
+    printf("3 - Verificar situacao:\n");
+    printf("4 - Exibir resultado:\n");
+    printf("5 - Calcular derivado:\n");
+    printf("6 - Sair:\n");
+
     switch (opcao)
     {
-    case 1: ;break;
+    case 1:
+    printf("Digite a nota 1:");
+    scanf("%d", &nota_1);
+    printf("Digite a nota 2");
+    scanf("%d", &nota_2);
+    prinft("notas salvas")
+    break;
     case 2: ;break;
     case 3: ;break;
     case 4: ;break;
