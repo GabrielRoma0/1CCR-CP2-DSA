69934dde12052c317802bf8342ca27ccef7b5839
Laboratório FIAP <labsfiap@fiap.com.br>
2026-04-22 22:08:28 -0300

    Código corrigido

--- src/codigo_com_erros.c
+++ src/codigo_com_erros.c
@@ -1,7 +1,7 @@
-#include "stdio.h"
+#include <stdio.h>
 
 int main() {
-    float nota1, nota2, media
+    float nota1, nota2, media;
 
     printf("Digite a primeira nota: ");
     scanf("%f", &nota1);
@@ -9,9 +9,9 @@ int main() {
     printf("Digite a segunda nota: ");
     scanf("%f", nota2);
 
-    media = nota1 + nota2 / 2;
+    media = (nota1 + nota2) / 2;
 
-    if (media = 6) {
+    if (media >= 6) {
         printf("Aluno aprovado\n");
     } else {
         printf("Aluno reprovado\n");
